56aad293cd54c1e167307eb5d7bda63fd7f32f01
Albert <alber725@gmail.com>
2021-04-19 15:37:22 -0300

    test: create a user

---
+++ src/modules/users/useCases/CreateUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,90 @@
+/* eslint-disable max-classes-per-file */
+import { ICreateUserDTO } from '@modules/users/dtos/ICreateUserDTO';
+import { IUsersRepository } from '@modules/users/infra/typeorm/repositories/protocol/IUsersRepositories';
+import { User } from '@modules/users/infra/typeorm/schema/User';
+import { AppError } from '@shared/errors/AppError';
+import { ICpfValidatorProvider } from '@shared/providers/CpfValidatorProvider/protocol/ICpfValidatorProvider';
+
+import { CreateUserUseCase } from './CreateUserUseCase';
+
+interface ISutTypes {
+  sut: CreateUserUseCase;
+  usersRepositoryStub: IUsersRepository;
+  cpfValidatorStub: ICpfValidatorProvider;
+}
+
+const makeCpfValidator = (): ICpfValidatorProvider => {
+  class CpfValidatorStub implements ICpfValidatorProvider {
+    isValid(cpf: string): boolean {
+      return true;
+    }
+  }
+
+  return new CpfValidatorStub();
+};
+
+const makeUsersRepository = (): IUsersRepository => {
+  class UsersRepositoryStub implements IUsersRepository {
+    async create(data: ICreateUserDTO): Promise<User> {
+      const fakeUser = {
+        id: 'valid_id',
+        name: 'valid_name',
+        email: 'any_mail@mail.com',
+        password: 'hashed_password',
+        cpf: '123456',
+        cep: 123456,
+        street: 'valid_street',
+        homeNumber: 100,
+        district: 'valid_district',
+        city: 'valid_city',
+        state: 'valid_state',
+        country: 'valid_country',
+      };
+
+      return new Promise(resolve => resolve(fakeUser));
+    }
+
+    findByEmail(email: string): Promise<User | undefined> {
+      return new Promise(resolve => resolve(undefined));
+    }
+  }
+
+  return new UsersRepositoryStub();
+};
+
+const makeSut = (): ISutTypes => {
+  const usersRepositoryStub = makeUsersRepository();
+  const cpfValidatorStub = makeCpfValidator();
+
+  const sut = new CreateUserUseCase(usersRepositoryStub, cpfValidatorStub);
+
+  return {
+    usersRepositoryStub,
+    sut,
+    cpfValidatorStub,
+  };
+};
+
+describe('Users', () => {
+  it('Should be able to create a valid user', async () => {
+    const { sut } = makeSut();
+
+    const fakeUser = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'hashed_password',
+      cpf: '123456',
+      cep: 123456,
+      street: 'valid_street',
+      homeNumber: 100,
+      district: 'valid_district',
+      city: 'valid_city',
+      state: 'valid_state',
+      country: 'valid_country',
+    };
+
+    const user = await sut.execute(fakeUser);
+
+    expect(user).toHaveProperty('id');
+  });
+});
