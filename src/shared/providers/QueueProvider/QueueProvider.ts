72e892878aaf96e33f57e1892699465791b92280
Albert <alber725@gmail.com>
2021-04-26 15:12:49 -0300

    feat: queue implementation

--- src/shared/providers/QueueProvider/QueueProvider.ts
+++ src/shared/providers/QueueProvider/QueueProvider.ts
@@ -1,26 +1,46 @@
 import Queue from 'bull';
 
 import redisConfig from '@config/cache';
+import {
+  ISendMailSource,
+  SendMailUseCase,
+} from '@shared/providers/QueueProvider/SendMailService/implementations/SendMailUseCase';
 
-import jobs from './index';
 import { IQueueProvider } from './protocol/IQueueProvider';
 
-export class QueueProvider implements IQueueProvider {
-  private queues = Object.values(jobs).map(job => ({
-    bull: new Queue(job.key, { redis: redisConfig }),
-    name: job.key,
-    execute: job.execute,
-  }));
+const sendMail = new SendMailUseCase();
 
-  async addQueue(queueName: string, data: any): Promise<Queue.Job | undefined> {
+export class QueueProvider implements IQueueProvider {
+  constructor(
+    private queues = [
+      {
+        bull: new Queue(sendMail.key, {
+          redis: redisConfig,
+        }),
+        name: sendMail.key,
+        handle: sendMail.execute,
+      },
+    ],
+  ) {}
+
+  async addQueue(
+    queueName: string,
+    data: ISendMailSource,
+  ): Promise<Queue.Job | undefined> {
     const queue = this.queues.find(queue => queue.name === queueName);
 
-    return queue?.bull.add(data);
+    const { name, token, email } = data;
+
+    const responseParameters = this.queues.map(queue =>
+      queue.handle({ name, token, email }),
+    );
+
+    return queue?.bull.add(responseParameters);
   }
 
   async processQueue(): Promise<void> {
     this.queues.forEach(async queue => {
-      await queue.bull.process(queue.execute);
+      await queue.bull.process(() => queue.handle);
 
       queue.bull.on('failed', (job, err) => {
         console.log('Job failed', queue.name, job.data);
--- src/shared/providers/QueueProvider/protocol/IQueueProvider.ts
+++ src/shared/providers/QueueProvider/protocol/IQueueProvider.ts
@@ -1,6 +1,11 @@
 import Queue from 'bull';
 
+import { ISendMailSource } from '@shared/providers/QueueProvider/SendMailService/implementations/SendMailUseCase';
+
 export interface IQueueProvider {
-  addQueue(queueName: string, data: any): Promise<Queue.Job | undefined>;
+  addQueue(
+    queueName: string,
+    data: ISendMailSource,
+  ): Promise<Queue.Job | undefined>;
   processQueue(): Promise<void>;
 }
